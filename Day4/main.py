158e726468de28821d0f782b5905e01410b7fa25
Leylan Aurakeasamy <leylan.aurakeasamy1@gmail.com>
2020-12-05 14:03:16 +0200

    Part1 done

--- Day4/main.py
+++ Day4/main.py
@@ -1,4 +1,4 @@
-
+valid = ['byr','iyr','eyr','hgt','hcl','ecl','pid']
 
 with open('input.txt') as reader:
     data = reader.readlines()
@@ -12,7 +12,22 @@ for line in data:
         buffer_= ""
     else:
         buffer_ =buffer_ + line
-combined_data = [x.rstrip() for x in combined_data]
+combined_data.append(buffer_) #forgot about last one
+#combined_data = [x.rstrip() for x in combined_data]
+
+def check_passport(passport):
+  for word in valid:
+    if(passport.find(word)==-1):
+      print(passport.find(word))
+      return 0
+  return 1
 
 
+valid_count=0
+for passport in combined_data:
+  print(passport)
+  valid_count+=check_passport(passport)
+  print(f'<-----{valid_count}------>')
+       
+print(valid_count)
 
