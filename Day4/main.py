be388411bd5dc5b3811239f97c037fc7e6a7c3db
Leylan <leylan.aurakeasamy1@gmail.com>
2020-12-07 12:56:06 +0200

    Solve changed to count all the true instead of constantly checking the False.

--- Day4/main.py
+++ Day4/main.py
@@ -21,27 +21,63 @@ def check_passport(passport):
       return False
   return True
 def check_data(array):
-  for entry in array:
-    if(entry[0] == valid[0]): #byr
-      if(int(entry[1])< 1920 or int(entry[1]) > 2002):
-        return False
-    else if(entry[0] == valid[1]): #iyr
-      if(int(entry[1])< 2010 or int(entry[1]) > 2020):
-        return False
-    else if(entry[0] == valid[2]): #eyr
-      if(int(entry[1])< 2020 or int(entry[1]) > 2030):
-        return False
-    else if(entry[0] == valid[3]): #hgt
-      if(entry[1].find('cm') != -1):
-        convert = int(entry[1][:-2])
-    else if(entry[0] == valid[4]): #hcl
-
-    else if(entry[0] == valid[5]): #ecl
-
-    else if(entry[0] == valid[6]): #pid
-
-    else:
-      print("Fake News")
+    print("<------NEW-------->")
+    for entry in array:
+        print(f'---------{entry}---------------')
+        if(entry[0] == valid[0]): #byr
+            if(int(entry[1])< 1920 or int(entry[1]) > 2002):
+                print("***byr false")
+                return False
+        elif(entry[0] == valid[1]): #iyr
+            if(int(entry[1])< 2010 or int(entry[1]) > 2020):
+                print("***iyr false")
+                return False
+        elif(entry[0] == valid[2]): #eyr
+            if(int(entry[1])< 2020 or int(entry[1]) > 2030):
+                print("***eyr false")
+                return False
+        elif(entry[0] == valid[3]): #hgt
+            if(entry[1].find('cm') ==-1 and entry[1].find('in')==-1):
+                return False
+            convert = int(entry[1][:-2])
+            if(entry[1].find('cm') != -1):
+                if(convert<150 or convert >193):
+                    print("***hgt cm false")
+                    return False
+            if(entry[1].find('in') != -1):
+                if(convert<59 or convert >76):
+                    print("***hgt in false")
+                    return False
+        elif(entry[0] == valid[4]): #hcl
+            if(entry[1][0] != '#'):
+                print("***hcl hashtag false")
+                return False
+            if(len(entry[1][1:])!=6):
+                print("***hcl not 6 false")
+                return False
+            var = entry[1][1:4]
+            
+            for n in var:
+                if(n.isdigit()):
+                    if int(n) not in range(0,10):
+                        print("***Not in range number")
+                        return False
+                else:
+                    if n > 'f':
+                        print("***Not in range letter")
+                        return False
+        elif(entry[0] == valid[5]): #ecl
+            valid_eye = ['amb','blu','brn','gry','grn','hzl','oth']
+            if entry[1] not in valid_eye:
+                print('***Eye colour not found')
+                return False
+            #print(entry[1])
+        elif(entry[0] == valid[6]): #pid
+            if(len(entry[1])!=9):
+                print("***pid length incorrect")
+                return False
+    print("RETURN TRUE")    
+    return True
 
 #valid_count=0
 valid_passports=[]
@@ -54,3 +90,11 @@ valid_passports = [x.replace('\n',' ') for x in valid_passports]
 valid_passports = [x.split() for x in valid_passports]
 valid_passports = [[y.split(':') for y in x] for x in valid_passports]
 #valid_passports = [valid_passports.remove(passports) if(check_data(passports)==False) for passports in valid_passports]
+count=0
+for passports in valid_passports:
+    print(check_data(passports))
+    if(check_data(passports)==True):
+        count+=1
+        print(f'COUNTE == {count}')
+print(f'<---------------{count}------------------->')
+            
